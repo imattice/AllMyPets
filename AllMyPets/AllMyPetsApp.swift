eaa4732f3acadc3492fee039ad63e049b2559f67
Ike Mattice <ikemattice@gmail.com>
2020-10-06 11:46:51 -0700

    Initial Commit

---
+++ AllMyPets/AllMyPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllMyPetsApp.swift
+//  AllMyPets
+//
+//  Created by Ike Mattice on 10/6/20.
+//
+
+import SwiftUI
+
+@main
+struct AllMyPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllMyPets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllMyPets
+//
+//  Created by Ike Mattice on 10/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
